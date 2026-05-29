8ccad110c83c5fbf39314923624149f3db459c8b
Nawaf-cyber <imnawaf1290@gmail.com>
2026-05-29 15:27:28 +0300

    إزالة مجلد .next من التتبع

--- .next/dev/static/chunks/app_layout_tsx_004glpo._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/app_globals_0jn8.0u.css","static/chunks/node_modules_042v64l._.js","static/chunks/components_0y8~4an._.js"],
-    source: "dynamic"
-});
